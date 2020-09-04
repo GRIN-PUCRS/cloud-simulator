96eadcb17072e737107f8e6893f97e8ac74d9642
Paulo Severo <opaulosevero@gmail.com>
2020-09-04 08:13:50 -0300

    Adding paper example figure params

--- simulator/misc/constants.py
+++ simulator/misc/constants.py
@@ -3,12 +3,12 @@
 ###############################
 SAVE_TIME = 10 # Amount of time it takes to save the state of a VM
 RESTORE_TIME = 10 # Amount of time it takes to restore the state of a VM
-NETWORK_BW = 1024 # Network bandwidth (PAPER FIGURE EXAMPLE)
-# NETWORK_BW = 125 # Network bandwidth (in this case, 1Gbit)
+# NETWORK_BW = 1024 # Network bandwidth (PAPER FIGURE EXAMPLE)
+NETWORK_BW = 125 # Network bandwidth (in this case, 1Gbit) (PAPER EXPERIMENTS PARAMETER)
 
 
 ########################
 ## Server Maintenance ##
 ########################
-PATCHING_TIME = 60 # Amount of time it takes to apply a patch to a server (PAPER FIGURE EXAMPLE)
-# PATCHING_TIME = 180 # Amount of time it takes to apply a patch to a server
+# PATCHING_TIME = 60 # Amount of time it takes to apply a patch to a server (PAPER FIGURE EXAMPLE)
+PATCHING_TIME = 180 # Amount of time it takes to apply a patch to a server (PAPER EXPERIMENTS PARAMETER)
